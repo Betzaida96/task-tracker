21b9ccab1531c18281a0761c9092c696074ac807
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-03 14:40:48 -0600

    delete-draganddrop-datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,15 +76,40 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
+  const taskId = $(event.target).data("id");
 
+  taskList = taskList.filter(task => task.id !==taskId);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+
+  renderTaskList();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+  const taskId = ui.draggable.data("id");
+  const newStatus = $(event.target).attr("id").replace("-cards", "");
 
+  taskList = taskList.map(task => {
+    if (task.id === taskId) {
+      return { ...task, status: newStatus};
+    }
+    return task;
+  });
+  
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+  renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  renderTaskList();
 
+  $(".lane").droppable({
+    accept:".task-card",
+    drop: handleDrop
+  });
+
+  $("#taskDueDate").datepicker();
+
+  $("#taskForm").on("submit", handleAddTask);
 });
